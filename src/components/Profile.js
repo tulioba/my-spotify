abb06a7e94874fbef79c7b52f4a6fcf7dcd1d513
tulio <tulio.amorimm@gmail.com>
2022-10-22 22:00:43 -0300

    add req 13

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from './Header';
 // import Loading from './Loading';
 import { getUser } from '../services/userAPI';
+
+// const img = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoF6kOeJp_8PRe8aA7cjDde2hSYKWP5f94EQ&usqp=CAU';
 
 class Profile extends React.Component {
   constructor() {
@@ -30,13 +33,16 @@ class Profile extends React.Component {
     console.log(userInfo.name);
     console.log(userInfo.email);
     console.log(userInfo.description);
-    console.log(userInfo.image);
+    // console.log(userInfo.image);
     return (
       <div data-testid="page-profile">
         <Header />
         <div>
-          {/* { userInfo.map((user) => <h3 key={ user.name }>{ user.name }</h3>)} */}
-          <h2>to de ult</h2>
+          <h2>{ userInfo.name }</h2>
+          <h2>{ userInfo.email }</h2>
+          <h2>{ userInfo.description }</h2>
+          <img src="url-to-image" alt="chaves" data-testid="profile-image" />
+          <Link to="/profile/edit" data-testid="link-to-profile">Editar perfil</Link>
         </div>
       </div>
     );
