45d5c0e1ad61363309f762363e809a487af0cb1e
tulio <tulio.amorimm@gmail.com>
2022-10-22 21:20:31 -0300

    resolve lint req 12

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import Header from './Header';
+// import Loading from './Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      // showLoading: false,
+      userInfo: [],
+    };
+  }
+
+  async componentDidMount() {
+    // this.setState({
+    //   showLoading: true,
+    // });
+    const user = await getUser();
+    // console.log(user);
+    this.setState({
+      // showLoading: false,
+      userInfo: user,
+    });
+  }
+
   render() {
+    const { userInfo } = this.state;
+    console.log(userInfo);
     return (
       <div data-testid="page-profile">
         <Header />
+        <div>
+          { userInfo.map((user) => <h3 key={ user.name }>{ user.name }</h3>)}
+        </div>
       </div>
     );
   }
